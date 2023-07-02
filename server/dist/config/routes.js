311efb3b492ecf81af08da58c6cd4cd30cb3230f
JScodeKiddie <JScodeKiddie@gmail.com>
2023-07-02 11:58:04 +0200

    update removing old dist folder from the github repo

--- server/dist/config/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const controllers_module_1 = require("../controllers/controllers.module");
-const router = (app) => {
-    app.use('/api/users', controllers_module_1.userRouter);
-    app.use('/api/recipes', controllers_module_1.recipeRouter);
-};
-exports.router = router;
