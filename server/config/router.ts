3625525676797971531ccb945b9c547f92424996
JScodeKiddie <JScodeKiddie@gmail.com>
2023-07-02 17:34:01 +0200

    add client side base files for angular

--- server/config/router.ts
+++ server/config/router.ts
@@ -1,9 +1,11 @@
 import {
   recipeRouter as recipeController,
   userRouter as userController,
-} from '../controllers/controllers.module';
+} from "../controllers/controllers.module";
+
+import { Express } from "express";
 
 export const router = (app: Express): void => {
-  app.use('/api/users', userController);
-  app.use('/api/recipes', recipeController);
+  app.use("/api/users", userController);
+  app.use("/api/recipes", recipeController);
 };
