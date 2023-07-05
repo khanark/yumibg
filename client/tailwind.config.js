6143bc9926ff297ee228e7bbbabec8febffb8e21
JScodeKiddie <JScodeKiddie@gmail.com>
2023-07-05 21:59:31 +0200

    update home page

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      rancho: ["Rancho", "cursive"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
