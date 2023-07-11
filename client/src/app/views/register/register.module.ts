485acd19c0c74d21cdc8cca12e36f484b5face9e
JScodeKiddie <JScodeKiddie@gmail.com>
2023-07-11 17:20:10 +0200

    create register page

--- client/src/app/views/register/register.module.ts
+++ client/src/app/views/register/register.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [RegisterComponent],
 })
 export class RegisterModule {}
